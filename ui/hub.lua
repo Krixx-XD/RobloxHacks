1861838c17b50978e8a1b0a5861d5a022b9562a1
The DV7s <161109688+TheDV7s@users.noreply.github.com>
2024-06-22 21:50:38 +0500

    Create hub.lua

---
+++ ui/hub.lua
@@ -0,0 +1,6 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheDV7s/RobloxHacks/main/ui/kavo.lua"))()
+--main(not fully built)
+local Window = Library.CreateLib("DV7 Hub", "DarkTheme")
+local Section = Tab:NewSection("Main")
+Section:UpdateSection("Main(HUB)")
+--finish later
