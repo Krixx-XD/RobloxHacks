a1fd20d039aa72730dd6f554a8e895860f16aa00
The DV7s <161109688+TheDV7s@users.noreply.github.com>
2024-06-26 15:57:01 +0500

    Update DV7Hub.lua

--- init/DV7Hub.lua
+++ init/DV7Hub.lua
@@ -1,5 +1,5 @@
-local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
-local Window = OrionLib:MakeWindow({Name = "DV7 Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "DV7Errors"})
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))() --path to execute
+local Window = OrionLib:MakeWindow({Name = "DV7 Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "DV7Errors" IntoText = "DV7 Hub"})
 OrionLib:MakeNotification({
 	Name = "DV7 Hub",
 	Content = "Welcome to DV7 Hub!",
@@ -13,12 +13,12 @@ local Tab = Window:MakeTab({
 	PremiumOnly = false
 })
 local Section = Tab:AddSection({
-	Name = "Main"
+	Name = "Hot Scripts"
 })
 Tab:AddButton({
 	Name = "The Rake Remastered",
 	Callback = function()
-      loadstring(game:httpGet("https://raw.githubusercontent.com/TheDV7s/RobloxHacks/main/scripts/rake_re.lua"))()
+      loadstring(game:httpGet("https://raw.githubusercontent.com/TheDV7s/RobloxHacks/main/scripts/rake_re.lua"))() --path to execute
       OrionLib:MakeNotification({
 	Name = "Executed!",
 	Content = "The Rake [DV7 Hub] Has been executed!",
@@ -30,10 +30,25 @@ Tab:AddButton({
 Tab:AddButton({
 	Name = "MM2 Script",
 	Callback = function()
-      loadstring(game:httpGet("https://raw.githubusercontent.com/TheDV7s/RobloxHacks/main/scripts/mm2.lua"))()
+      loadstring(game:httpGet("https://raw.githubusercontent.com/TheDV7s/RobloxHacks/main/scripts/mm2.lua"))() --path to execute
       OrionLib:MakeNotification({
 	Name = "Executed!",
 	Content = "MM2 Script Has been executed!",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+  	end    
+})
+local Section = Tab:AddSection({
+		Name = "FPS Related"
+	})
+Tab:AddButton({
+	Name = "FPS Booster",
+	Callback = function()
+      loadstring(game:httpGet("https://raw.githubusercontent.com/TheDV7s/RobloxHacks/main/scripts/FPS.lua"))() --path to execute
+      OrionLib:MakeNotification({
+	Name = "Executed!",
+	Content = "FPS Booster Has been executed!",
 	Image = "rbxassetid://4483345998",
 	Time = 5
 })
