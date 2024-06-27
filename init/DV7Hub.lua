c22974d36bb14e92a6038722bf38c6a58639559e
The DV7s <161109688+TheDV7s@users.noreply.github.com>
2024-06-27 15:27:21 +0500

    Update DV7Hub.lua

--- init/DV7Hub.lua
+++ init/DV7Hub.lua
@@ -8,7 +8,7 @@ Rayfield:Notify({
       Ignore = {
          Name = "Okay!",
          Callback = function()
-         print("Exit Was Tapped Smh")
+         print("Okay Was Tapped Smh")
       end
    },
 },
@@ -63,7 +63,7 @@ Main:CreateButton({
    end,
 })
 
-local ClientTab = indow:CreateTab("HotScripts", 4483345998) -- Title, Image
+local ClientTab = Window:CreateTab("Client", 4483345998) -- Title, Image
 
 	ClientTab:CreateToggle({
 		Name = "FreeCam",
@@ -117,5 +117,98 @@ local ClientTab = indow:CreateTab("HotScripts", 4483345998) -- Title, Image
 					game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character.Head
 				end)
 			end
+		end,
+	})
+	ClientTab:CreateSlider({
+		Name = "FreeCam Speed",
+		Range = {0.01, 20},
+		Increment = 0.01,
+		CurrentValue = 0.2,
+		Flag = "Speed",
+		Callback = function(Value)
+			_G.FreeCamSpeed = Value
+		end,
+	})
+	ClientTab:CreateToggle({
+		Name = "Player ESP",
+		CurrentValue = false,
+		Flag = "PlrEsp",
+		Callback = function(state)
+			_G.PlayerESP = state
+			if _G.PlayerESP == true then
+				repeat wait()
+
+				until _G.PlayerESP == false
+				for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+					pcall(function()
+						if not v.Character then
+						else
+							if v.Character:GetAttribute("PlayerESP") == true then
+								v.Character:SetAttribute("PlayerESP",false)
+							end
+							if v.Character:FindFirstChild("PlayerChams") then
+								v.Character.PlayerChams:Remove()
+
+							end
+						end
+					end)
+				end
+			end
+		end,
+	})
+local PlayerTab = Window:CreateTab("Player", 11252440515)
+local ExploitsTab = Window:CreateTab("Settings", 11252440305)
+	PlayerTab:CreateToggle({
+		Name = "No Fall Damage",
+		CurrentValue = false,
+		Flag = "NoFallDamage",
+		Callback = function(state)
+			_G.NoFallDMG = state
+		end,
+	})
+
+	PlayerTab:CreateSlider({
+		Name = "Field Of View",
+		Range = {0, 120},
+		Increment = 1,
+		CurrentValue = 70,
+		Flag = "FOV",
+		Callback = function(state)
+			_G.FieldOfView=state
+		end,
+	})
+
+	PlayerTab:CreateToggle({
+		Name = "Toggle FOV",
+		CurrentValue = false,
+		Flag = "tglFOV",
+		Callback = function(state)
+			_G.enableFOV = state
+		end,
+	})
+	
+	PlayerTab:CreateSlider({
+		Name = "WalkSpeed",
+		Range = {0, 30},
+		Increment = 1,
+		CurrentValue = 16,
+		Flag = "walkspeed",
+		Callback = function(state)
+			_G.WalkSpeedd=state
+		end,
+	})
+
+	PlayerTab:CreateToggle({
+		Name = "Toggle WalkSpeed",
+		CurrentValue = false,
+		Flag = "tglSpeed",
+		Callback = function(state)
+			_G.enableSpeed = state
+		end,
+	})
+	SettingsTab:CreateButton({
+		Name = "Unload Gui",
+		Callback = function()
+			DestroyUI()
 		end,
 	})
