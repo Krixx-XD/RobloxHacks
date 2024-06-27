838f2b213a93a81b4b4673fb9366069c4be071c7
The DV7s <161109688+TheDV7s@users.noreply.github.com>
2024-06-27 14:03:53 +0500

    Update DV7Hub.lua

--- init/DV7Hub.lua
+++ init/DV7Hub.lua
@@ -1,144 +1,121 @@
-local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
-local Window = OrionLib:MakeWindow({Name = "DV7 Hub", HidePremium = false, IntoEnabled = true, IntroText = "DV7 Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
---Notification
-OrionLib:MakeNotification({
-	Name = "DV7 Hub",
-	Content = "Welcome to DV7 Hub, Here you will find almost every script(s) u need!",
-	Image = "rbxassetid://4483345998",
-	Time = 5
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+Rayfield:Notify({
+   Title = "Welcome ",
+   Content = "Notification Content",
+   Duration = 6.5,
+   Image = 4483362458,
+   Actions = { -- Notification Buttons
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+         print("Exit Was Tapped Smh")
+      end
+   },
+},
+})
+local Window = Rayfield:CreateWindow({
+   Name = "DV7 Hub",
+   LoadingTitle = "DV7 Hub",
+   LoadingSubtitle = "by The DV7s",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "DV7 Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "DV7 Hub",
+      Subtitle = "Key System",
+      Note = "Join our discord sever and head over to #key",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"DV7Team","balls","a","DV7HubTop","Admin","Owner","Special","Ray","Gai","x","Miyo"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
 
+local Main = Window:CreateTab("HotScripts", 11252440817) -- Title, Image
 
---Tabs
-local HotScripts = Window:MakeTab({
-	Name = "Hot Scripts",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+Main:CreateButton({
+   Name = "MurdererMystery2",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/TheDV7s/RobloxHacks/main/scripts/mm2.lua"))()
+      print("Executed")
+   end,
+})
+Main:CreateButton({
+   Name = "The Rake Remastered",
+   Callback = function()
+      loadstring(game:HttpGet("https://dv-7.w3spaces.com/s.txt"))()
+      print("Executed")
+   end,
+})
+Main:CreateButton({
+   Name = "FPS Booster(Might Remove Important Assets)",
+   Callback = function()
+      loadstring(game:HttpGet("https://dv-7.w3spaces.com/q.txt"))()
+      print("Executed")
+   end,
 })
 
---Buttons
-HotScripts:AddButton({
-	Name = "The Rake Remastered",
-	Callback = function()
-      		loadstring(game:httpGet("https://raw.githubusercontent.com/TheDV7s/RobloxHacks/main/scripts/rake_re.lua"))()
-            print("Executed")
-            warn("Executed The Rake Remastered Script")
-  	end    
-})
+local ClientTab = indow:CreateTab("HotScripts", 4483345998) -- Title, Image
 
+	ClientTab:CreateToggle({
+		Name = "FreeCam",
+		CurrentValue = false,
+		Flag = "freeCam",
+		Callback = function(state)
+			_G.FreeCam = state
+			if _G.FreeCam == true then
+				pcall(function()
 
-HotScripts:AddButton({
-        Name = "Murderer Mystery 2",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/TheDV7s/RobloxHacks/main/scripts/mm2.lua"))()
-            print("Executed")
-            warn("Executed Murderer Mystery 2 Script")
-        end
-    })
+					FreeCamPart.CFrame = game.Players.LocalPlayer.Character.Head.CFrame
+				end)
+				repeat wait()
+					pcall(function()
+						function tweenPart(time,partto,TSCF)
+							local tweenService = game:GetService("TweenService")
+							local tweenInfo = TweenInfo.new(time, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0)
+							local part = partto
+							local goal = {}
+							goal.CFrame = TSCF
+							local tween = tweenService:Create(part, tweenInfo, goal)
+							tween:Play()
+						end
+						game.Workspace.CurrentCamera.CameraType = Enum.CameraType.Scriptable
+						local mouse = game.Players.LocalPlayer:GetMouse()
+						game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Anchored = true
+						game.Workspace.CurrentCamera.CameraSubject = FreeCamPart
 
-local PlayerScripts = Window:MakeTab({
-        Name = "Player Scripts",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
---Buttons
-PlayerScripts:AddButton({
-        Name = "FE Invisible",
-        Callback = function()
-            local player = game.Players.LocalPlayer
-            position     = player.Character.HumanoidRootPart.Position
-            wait(0.1)
-            player.Character:MoveTo(position + Vector3.new(0, 1000000, 0))
-            wait(0.1)
-            humanoidrootpart = player.Character.HumanoidRootPart:clone()
-            wait(0.1)
-            player.Character.HumanoidRootPart:Destroy()
-            humanoidrootpart.Parent = player.Character
-            player.Character:MoveTo(position)
-            wait()
-            warn("Done!!!!")
-        end
-    })
-PlayerScripts:AddButton({
-        Name = "FE Invis Fling Players(R15 ONLY)",
-        Callback = function()
-            local destroy_almost_all_body = false
-local remove_accessory = false
-
--- Script
-
-local function InvisFunctionR15() -- invis function
-	
-	local function invisible()
-		for i, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do -- destroy lowertorso motor6d
-			if v.Name == "LowerTorso" then
-				for i, vv in pairs(v:GetChildren()) do
-					if vv:IsA("Motor6D") then
-						vv:Destroy()
-					end
-				end
-			end
-		end
-	end
-	
-	local function destroy_all()
-		for i, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do -- destroy other motor6d
-			if v.Name ~= "HumanoidRootPart" and v:IsA("MeshPart") then	
-				for i, vv in v:GetChildren() do
-					if vv:IsA("Motor6D") and v.Name ~= "Head" then
-						vv:Destroy()
-					end
-				end
-
-				if v.Name == "LowerTorso" then
-					v:Destroy()
-				end
-			end
-		end
-	end
-	
-	local function remove_acessory_sc()
-		for i, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do -- destroy accesories
-			if v:IsA("Accessory") then
-				for i, vv in pairs(v:GetChildren()) do
-					if vv.Name == "Handle" then
-						for i, weld in pairs(vv:GetChildren()) do
-							if weld:IsA("Weld") then
-								weld:Destroy()
+						FreeCamPart.CFrame = CFrame.new(FreeCamPart.Position,mouse.Hit.Position)
+						if InputService:IsKeyDown(Enum.KeyCode.Space) then
+							tweenPart(0.04,FreeCamPart,FreeCamPart.CFrame + Vector3.new(0,_G.FreeCamSpeed,0))
+							--FreeCamPart.CFrame = FreeCamPart.CFrame + Vector3.new(0,_G.FreeCamSpeed,0)
+						elseif InputService:IsKeyDown(Enum.KeyCode.LeftShift) then
+							tweenPart(0.04,FreeCamPart,FreeCamPart.CFrame + Vector3.new(0,-_G.FreeCamSpeed,0))
+							--FreeCamPart.CFrame = FreeCamPart.CFrame + Vector3.new(0,-_G.FreeCamSpeed,0)
+						end
+						if true then
+							if InputService:IsKeyDown(Enum.KeyCode.W) then
+								tweenPart(0.04,FreeCamPart,FreeCamPart.CFrame+FreeCamPart.CFrame.LookVector * _G.FreeCamSpeed)
+								-- FreeCamPart.CFrame = FreeCamPart.CFrame+FreeCamPart.CFrame.LookVector * _G.FreeCamSpeed
+							elseif InputService:IsKeyDown(Enum.KeyCode.S) then
+								tweenPart(0.04,FreeCamPart,FreeCamPart.CFrame+FreeCamPart.CFrame.LookVector * -_G.FreeCamSpeed)
+								-- FreeCamPart.CFrame = FreeCamPart.CFrame+FreeCamPart.CFrame.LookVector * -_G.FreeCamSpeed
 							end
 						end
-					end
-				end
+					end)
+				until _G.FreeCam == false
+				pcall(function()
+					game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+					game.Workspace.CurrentCamera.CameraType = Enum.CameraType.Custom
+					game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character.Head
+				end)
 			end
-		end
-	end
-	
-	invisible()
-	
-	task.wait(1)
-	
-	if destroy_almost_all_body then
-		destroy_all()
-	end
-	
-	if remove_accessory then
-		remove_acessory_sc()
-	end
-end
-
-InvisFunctionR15()
-        end
-    })
-
-local PopularScripts = Window:MakeTab({
-        Name = "Popular Scripts",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
-PopularScrips:AddButton({
-        Name = "Hoho Hub 15+ Games"
-        Callback = function()
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/ascn123/HOHO_H/main/Loading_UI'))()
-        end
-    })
-OrionLib:Init()
+		end,
+	})
