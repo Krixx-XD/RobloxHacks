a93740847326eec56bb9d28934b36124dc78d1c7
The DV7s <161109688+TheDV7s@users.noreply.github.com>
2024-06-27 18:14:10 +0500

    Update x89.lua

--- engine/scripts/x89.lua
+++ engine/scripts/x89.lua
@@ -1,1 +1,2 @@
-v={}game={}x64={}local a,b;v.d=function(...)if...==0 then v.Game(getDescendants())print("Name:"..v.Name)v.Game(getPlayerDevice())if v.Game==not v.GameGetPlayerDevice then v.Game(getPlayerDevice(),true)end end end;local c={"RobloxHacks.zip","engine","/","scripts","/"}local d={"C:","\\",c[1],"\\",c[2],"\\",c[4],"\\"}local e=os.remove(c,"useless.lua")local f,g=os.remove(d,"useless.lua")if f==v.failed then g=os.remove(d,"useless.lua")end
+---@private
+c={}v={}v.d=38;v.m=v.d+98;v.e=function()v.f=v.m+43 end;v.k=v.f+48;v.kd={Name=c.getUserId(),userId=translateToName(true)}v.ed=v.kd.userId-43;v.di=v.kd.userId*88;b={}f={}l={}z={}x={}n={}m={}d={}s={}a={}b.fo=v.kd.userId/106;f.ko=v.di+538;v.ff,v.gt,v.ri,x.fe,v.dffe=3+3+8+4+9+49
